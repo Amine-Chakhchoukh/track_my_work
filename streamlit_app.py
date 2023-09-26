6c7d2e1255aff090267f62e426a205bf7f481501
Amine <49071600+Amine-Chakhchoukh@users.noreply.github.com>
2023-09-26 23:26:00 +0100

    add time tracker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 
 # ################################################################
 st.set_page_config(page_title="My work tracking app!", 
@@ -18,16 +19,20 @@ if not st.session_state["input_form_clicked"]:
     with placeholder.form("input_form"):
         htp5="https://lh3.googleusercontent.com/pw/AIL4fc-PpqW403juiRdL0TilMr8xF0tXLQgiYvB7TaAktqjOfs1FIlOYwhkOudAqv4sRhXpB8dIcLp6Q9PwDbZpA-Vk10EWCYmpcP0_-980sVr5Pjt2NORzSb7976tQZ2vukEzzffuGwaf9ABz9dz36veiOfJQ=w2002-h1502-s-no"
         st.image(htp5, width=300)
-        st.title("Dario!! We did it! We have a (Python) app!")
+        st.title("Let's track the time I leave the office!")
         
         response_dropdown = ('Yay!', 'Fantastic!')
         
-        a1 = st.selectbox('Can you believe it!',
+        a1 = st.selectbox('Have you left the office?',
                             response_dropdown)
     
-        submitted = st.form_submit_button("So cool!", on_click=callback)
+        submitted = st.form_submit_button("Yes!", on_click=callback)
+
+        if submitted:
+          st.write(f"You left at {datetime.utcnow()} UTC time")
+          
        
-        st.write("Made with 💚 by Amine in the living room!")
+        st.write("Made with 💚 by Amine in London!")
         
         if st.session_state["input_form_clicked"]:
             placeholder.empty()
