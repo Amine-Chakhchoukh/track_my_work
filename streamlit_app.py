6b4556e88a992dff77cc016e703708647593bb52
Amine <49071600+Amine-Chakhchoukh@users.noreply.github.com>
2023-09-26 23:40:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,10 @@ if not st.session_state["input_form_clicked"]:
         if st.session_state["input_form_clicked"]:
             placeholder.empty()
             
-if st.session_state["input_form_clicked"]:
-    # with placeholder.form("new_form"):
-    htp5="https://lh3.googleusercontent.com/pw/AIL4fc-u_AzKi0MTjL8-EAWJBNjO62Nlk3H5T9NTbJw0eJXsOkqDuZe7pTyhU6mWzmHMbTCnHEjU6MotCLRsc5PNGCJ8KbP83ehff-nQ6CzqIwl6J3Q8iviyIsQSKDRzfLYgLqHyCZdUmnORJKPaf4D-N-vzgw=w2002-h1502-s-no"
-    st.image(htp5, width=300)
-    # time = datetime.now().astimezone(timezone('Europe/UK'))
-    # st.write(f"You left at {time} UTC time")
+# if st.session_state["input_form_clicked"]:
+#     # with placeholder.form("new_form"):
+#     htp5="https://lh3.googleusercontent.com/pw/AIL4fc-u_AzKi0MTjL8-EAWJBNjO62Nlk3H5T9NTbJw0eJXsOkqDuZe7pTyhU6mWzmHMbTCnHEjU6MotCLRsc5PNGCJ8KbP83ehff-nQ6CzqIwl6J3Q8iviyIsQSKDRzfLYgLqHyCZdUmnORJKPaf4D-N-vzgw=w2002-h1502-s-no"
+#     st.image(htp5, width=300)
+#     # time = datetime.now().astimezone(timezone('Europe/UK'))
+#     # st.write(f"You left at {time} UTC time")
         
